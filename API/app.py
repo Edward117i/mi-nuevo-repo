6e89319ee98155a5c7c05e3023ad272e150178e6
Edward <sampatbm3@gmail.com>
2025-07-15 12:13:45 -0600

    Nuevo proyecto de api

---
+++ API/app.py
@@ -0,0 +1,18 @@
+import os
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/familia")
+def get_familia():
+    rows = ["Edward", "Laura", "Johan", "Priscilla"]
+    return rows
+
+@app.get("/superheroesDC")
+def get_superheroes():
+    rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
+    return rows
+
+
